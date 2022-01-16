ce08c46cd4111f2131e7747c02c6678addb13038
yr7000 <78471661+yr7000@users.noreply.github.com>
2022-01-16 16:38:31 +0530

    Time: 236 ms (32.60%), Space: 171.5 MB (11.29%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     vector<vector<string>> answer;
+    int dp[17][17];
     
     bool isPalindrome(string s,int startIndex,int endIndex){
-        while(startIndex<endIndex){
-            if(s[startIndex++]!=s[endIndex--]) return false;
-        }
-        return true;
+        if(startIndex>=endIndex) return true;
+        if(dp[startIndex][endIndex]!=-1) return dp[startIndex][endIndex];
+        if(s[startIndex]==s[endIndex]) return dp[startIndex][endIndex] = isPalindrome(s,startIndex+1,endIndex-1);
+        return dp[startIndex][endIndex] = false;
     }
     
     void getAnswer(string s,int startIndex,vector<string> partition){
@@ -26,6 +27,7 @@ public:
     
     vector<vector<string>> partition(string s) {
         vector<string> partition;
+        memset(dp,-1,sizeof(dp));
         getAnswer(s,0,partition);
         return answer;
     }
