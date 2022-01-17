c2f48792eee3c47f9a71fc8448df8d8c2e17f06d
yr7000 <78471661+yr7000@users.noreply.github.com>
2022-01-17 14:53:53 +0530

    Time: 61 ms (48.99%), Space: 9.8 MB (79.64%) - LeetHub

---
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    
+    int dx[3] = {-1,0,1};
+    int dp[71][71][71];
+    
+    int getAns(vector<vector<int>> &grid,int row,int c1,int c2,int n,int m){
+        if(row==n) return 0;
+        if(c1<0 or c2<0 or c1>=m or c2>=m) return INT_MIN;
+        if(dp[row][c1][c2]!=-1) return dp[row][c1][c2];
+        int ans = 0;
+        for(int i=0;i<3;i++){
+            int col1 = c1-dx[i];
+            for(int j=0;j<3;j++){
+                int col2 = c2-dx[j];
+                ans = max(ans,getAns(grid,row+1,col1,col2,n,m));
+            }
+        }
+        ans += c1==c2?grid[row][c1]:grid[row][c1]+grid[row][c2];
+        return dp[row][c1][c2] = ans;
+    }
+    
+    int cherryPickup(vector<vector<int>>& grid) {
+        int n = grid.size();
+        if(n==0) return 0;
+        int m = grid[0].size();
+        memset(dp,-1,sizeof(dp));
+        return getAns(grid,0,0,m-1,n,m);
+    }
+};
