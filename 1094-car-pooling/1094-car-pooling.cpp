b25c41c5291c662b9c1dabccbbd87258706d5e02
yr7000 <78471661+yr7000@users.noreply.github.com>
2022-01-17 14:53:02 +0530

    Time: 19 ms (53.94%), Space: 10.5 MB (25.45%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -2,11 +2,16 @@ class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
         int dropCount[1001] = {0};
+        int inCount[1001] = {0};
         for(auto trip:trips){
-            dropCount[trip[2]] -= trip[0];
-            dropCount[trip[1]] += trip[0];
+            dropCount[trip[2]] += trip[0];
+            inCount[trip[1]] += trip[0];
         }
-        for(int i=0;i<=1000 and capacity>=0;i++) capacity -= dropCount[i];
-        return capacity>=0;
+        for(int i=0;i<=1000;i++){
+            capacity += dropCount[i];
+            if(capacity<inCount[i]) return false;
+            capacity -= inCount[i];
+        }
+        return true;
     }
 };
