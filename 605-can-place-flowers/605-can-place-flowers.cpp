6724d3995106f614463e8c3b5cc9c118a5cdb9a8
yr7000 <78471661+yr7000@users.noreply.github.com>
2022-01-18 10:18:40 +0530

    Time: 42 ms (5.42%), Space: 20.2 MB (69.55%) - LeetHub

--- 605-can-place-flowers/605-can-place-flowers.cpp
+++ 605-can-place-flowers/605-can-place-flowers.cpp
@@ -3,17 +3,16 @@ public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
         int m = flowerbed.size();
         if(n==0) return true;
-        if(n>m) return false;
+        
         for(int i=0;i<m;i++){
-            if(flowerbed[i]) continue;
             int left = i>=1?flowerbed[i-1]:0;
             int right = i+1<m?flowerbed[i+1]:0;
-            if(left+right==0){
+            if(left+right+flowerbed[i]==0){
                 flowerbed[i] = 1;
                 n--;
                 if(n==0) return true;
             }
         }
-        return false;
+        return false;        
     }
 };
