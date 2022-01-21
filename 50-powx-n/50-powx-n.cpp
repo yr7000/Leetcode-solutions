a325be0b5ef3d5ecb142b2ef8bbd592fae70f4d7
yr7000 <78471661+yr7000@users.noreply.github.com>
2022-01-21 19:52:21 +0530

    Time: 6 ms (7.23%), Space: 6 MB (41.90%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
-    double myPow(double x, int n) {
-        if(n==0) return 1.0;
+    double myPow(double a, int n) {
         bool neg = n<0;
         n = abs(n);
-        double y = myPow(x,n/2);
-        double soln;
-        if(n&1) soln = x*y*y;
-        else soln = y*y;
+        double soln = 1.0;
+        while(n>0){
+            if(n&1){
+                soln *= a;
+            }
+            n >>= 1;
+            a = a*a;
+        }
         if(neg) return 1.0/soln;
         return soln;
     }
