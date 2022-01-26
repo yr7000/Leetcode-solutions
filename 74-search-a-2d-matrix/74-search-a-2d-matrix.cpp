963c668da529e8360249356de2dbf6c6e61c7310
yr7000 <78471661+yr7000@users.noreply.github.com>
2022-01-26 11:41:23 +0530

    Time: 7 ms (31.69%), Space: 9.3 MB (99.98%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
-    
-    bool binarySearch(vector<int> v,int target){
-        int l = 0, r = v.size()-1;
-        while(l<=r){
-            int m = l+(r-l)/2;
-            if(v[m] == target){
-                return true;
-            }else if(v[m]>target){
-                r = m-1;
-            }else{
-                l = m+1;
-            }
-        }
-        return false;
-    }
-    
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        for(int i=0;i<matrix.size();i++){
-            if(binarySearch(matrix[i],target)) return true;
+        int n = matrix.size(), m = matrix[0].size();
+        int l = 0, r = n*m-1;
+        while(l<=r){
+            int mid = l + (r-l)/2;
+            int val = matrix[mid/m][mid%m];
+            if(val == target) return true;
+            else if(val>target) r = mid-1;
+            else l = mid+1;
         }
         return false;
     }
