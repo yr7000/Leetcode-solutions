56f6550ccefd2fe6886582496a4b7dd487c60c4c
yr7000 <78471661+yr7000@users.noreply.github.com>
2022-02-08 23:01:38 +0530

    Time: 316 ms (7.95%), Space: 81.5 MB (5.14%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -1,47 +1,38 @@
 class Solution {
 public:
     
-    vector<int> Nsl(vector<int> &h){
+    vector<int> getNsl(vector<int> &A,int n){
+        vector<int> result(n);
         stack<int> s;
-        vector<int> ans;
-        s.push(0);
-        for(int i=1;i<h.size()-1;i++){
-            while(h[s.top()]>=h[i]){
-                s.pop();
-            }
-            ans.push_back(s.top());
+        for(int i=0;i<n;i++){
+            while(!s.empty() and A[s.top()]>=A[i]) s.pop();
+            if(s.empty()) result[i] = i;
+            else result[i] = i-s.top()-1;
             s.push(i);
         }
-        return ans;
+        return result;
     }
     
-    vector<int> Nsr(vector<int> &h){
+    vector<int> getNsr(vector<int> &A,int n){
+        vector<int> result(n);
         stack<int> s;
-        vector<int> ans;
-        s.push(h.size()-1);
-        for(int i=h.size()-2;i>=1;i--){
-            while(h[s.top()]>=h[i]){
-                s.pop();
-            }
-            ans.push_back(s.top());
+        for(int i=n-1;i>=0;i--){
+            while(!s.empty() and A[s.top()]>=A[i]) s.pop();
+            if(s.empty()) result[i] = (n-1)-i;
+            else result[i] = s.top()-i-1;
             s.push(i);
         }
-        reverse(ans.begin(),ans.end());
-        return ans;
+        return result;       
     }
     
     int largestRectangleArea(vector<int>& heights) {
-        vector<int> h;
-        h.push_back(-1);
-        for(auto i:heights) h.push_back(i);
-        h.push_back(-1);
-        vector<int> nsl = Nsl(h);
-        vector<int> nsr = Nsr(h);
+        vector<int> nsl = getNsl(heights,heights.size());
+        vector<int> nsr = getNsr(heights,heights.size());
         int ans = 0;
         for(int i=0;i<heights.size();i++){
             int mnl = nsl[i];
             int mnr = nsr[i]; 
-            ans = max(ans,(mnr-mnl-1)*heights[i]);
+            ans = max(ans,(mnr+mnl+1)*heights[i]);
         }
         return ans;
     }
