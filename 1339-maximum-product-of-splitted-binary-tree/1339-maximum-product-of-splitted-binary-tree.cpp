32d3f4392aaec6fcc5cd5fa893a055a585cce2d0
yr7000 <78471661+yr7000@users.noreply.github.com>
2022-02-18 23:38:31 +0530

    Time: 128 ms (76.46%), Space: 77.4 MB (70.84%) - LeetHub

--- 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.cpp
+++ 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.cpp
@@ -12,24 +12,16 @@
 # define mod 1000000007
 class Solution {
 public:
-    
-    void replaceValByProduct(TreeNode* root){
-        if(root==NULL) return;
-        replaceValByProduct(root->left);
-        replaceValByProduct(root->right);
-        if(root->left) root->val += root->left->val;
-        if(root->right) root->val += root->right->val;
-    }
-    long int getMaxProduct(TreeNode* root,long int totalSum){
-        if(root==NULL) return 1;
-        long int choise1 = (root->val * (totalSum-root->val));
-        long int choise2 = getMaxProduct(root->left,totalSum);
-        long int choise3 = getMaxProduct(root->right,totalSum);
-        return max({choise1,choise2,choise3});
+    long int result = 0, totalSum = 0;
+    long int dfs(TreeNode* root){
+        if(root==NULL) return 0;
+        long int currentSum = root->val + dfs(root->right) + dfs(root->left);
+        result = max(result,(currentSum * (totalSum-currentSum)));
+        return currentSum;
     }
     int maxProduct(TreeNode* root) {
-        replaceValByProduct(root);
-        long int totalSum = root->val;
-        return max(getMaxProduct(root->left,totalSum),getMaxProduct(root->right,totalSum))%mod;
+        totalSum = dfs(root);
+        dfs(root);
+        return result%(1000000007);
     }
 };
