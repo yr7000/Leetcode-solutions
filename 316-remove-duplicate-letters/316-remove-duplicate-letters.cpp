12ed44a515bc80364e224d17cb89e6b49af01922
yr7000 <78471661+yr7000@users.noreply.github.com>
2022-02-26 23:26:14 +0530

    Time: 5 ms (36.17%), Space: 6.5 MB (80.14%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     string removeDuplicateLetters(string s) {
-        vector<int> dict(256, 0);
-        vector<bool> visited(256, false);
-        for(auto ch : s)  dict[ch]++;
-        string result = "0";
-        for(auto c : s) {
-            dict[c]--;
-            if(visited[c])  continue;
-            while(c < result.back() && dict[result.back()]) {
-                visited[result.back()] = false;
+        vector<int> cnt(26, 0);
+        vector<bool> visited(26, false);
+        for(char ch : s)  cnt[ch-'a']++;
+        string result = "";
+        for(char c : s) {
+            cnt[c-'a']--;
+            if(visited[c-'a'])  continue;
+            while(!result.empty() and c < result.back() && cnt[result.back()-'a']) {
+                visited[result.back()-'a'] = false;
                 result.pop_back();
             }
             result += c;
-            visited[c] = true;
+            visited[c-'a'] = true;
         }
-        return result.substr(1);      
+        return result;      
     }
 };
