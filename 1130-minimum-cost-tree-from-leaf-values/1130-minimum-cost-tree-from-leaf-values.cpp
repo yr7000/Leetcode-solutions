4ae2a554d2c0f8090c9333ac567f61e1395e72e4
yr7000 <78471661+yr7000@users.noreply.github.com>
2022-03-03 22:24:42 +0530

    Time: 2 ms (85.61%), Space: 8.4 MB (64.45%) - LeetHub

--- 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
+++ 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
@@ -1,33 +1,40 @@
-#define vivi vector<vector<int>>
 class Solution {
 public:
-    
-    int dp(vector<int> &arr,int i,int j, vivi &memo ){
-        if(i>=j) return 0;
-        if(j-i == 1) return arr[i]*arr[j];
-        if(memo[i][j]!=-1) return memo[i][j];
-        
-        int sizeOfRange = j-i+1;
-        int maxRight[sizeOfRange];
-        maxRight[sizeOfRange-1] = arr[j];
-        for(int k = sizeOfRange-2;k>=0;k--){
-            maxRight[k] = max(maxRight[k+1],arr[k+i]);
-        }
-        
-        int bestResult = INT_MAX;        
-        int maxLeft = INT_MIN;
-        for(int k=i;k<j;k++){
-            maxLeft = max(maxLeft,arr[k]);
-            int mxRight = maxRight[k-i+1];
-            int choise = dp(arr,i,k,memo) + dp(arr,k+1,j,memo) + maxLeft*mxRight;
-            bestResult = min(bestResult,choise);
-        }
-        return memo[i][j] = bestResult;
-    }
-    
     int mctFromLeafValues(vector<int>& arr) {
         int n = arr.size();
-        vivi memo(n,vector<int>(n,-1));
-        return dp(arr,0,n-1,memo);
+        vector<int> adj[16];
+        for(int i=0;i<n;i++){
+            adj[arr[i]].push_back(i);
+        }
+        
+        int left[n], right[n];
+        for(int i=0;i<n;i++){
+            left[i] = i-1;
+            right[i] = i+1;
+        }
+        
+        int ans = 0;
+        for(int i=1;i<=15;i++){
+            for(int j=0;j<adj[i].size();j++){
+                int index = adj[i][j];
+                int bestPossibleNbr = 0;
+                if(left[index]==-1 and right[index]!=n){
+                    bestPossibleNbr = arr[right[index]];
+                }else if(left[index]!=-1 and right[index]==n){
+                    bestPossibleNbr = arr[left[index]];
+                }else if(left[index]!=-1 and right[index]!=n){
+                    bestPossibleNbr = min(arr[left[index]],arr[right[index]]);
+                }
+                
+                if(left[index]!=-1){
+                    right[left[index]] = right[index];
+                }
+                if(right[index]!=n){
+                    left[right[index]] = left[index];
+                }
+                ans += bestPossibleNbr*i;
+            }
+        }
+        return ans;
     }
 };
