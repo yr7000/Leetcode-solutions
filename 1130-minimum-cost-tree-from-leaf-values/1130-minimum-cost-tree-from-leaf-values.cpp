597fa8a195e0213e30b0086456d4995eea1f0e56
yr7000 <78471661+yr7000@users.noreply.github.com>
2022-03-03 22:44:24 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (64.45%) - LeetHub

--- 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
+++ 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
@@ -1,40 +1,26 @@
 class Solution {
 public:
     int mctFromLeafValues(vector<int>& arr) {
-        int n = arr.size();
-        vector<int> adj[16];
-        for(int i=0;i<n;i++){
-            adj[arr[i]].push_back(i);
-        }
-        
-        int left[n], right[n];
-        for(int i=0;i<n;i++){
-            left[i] = i-1;
-            right[i] = i+1;
-        }
-        
-        int ans = 0;
-        for(int i=1;i<=15;i++){
-            for(int j=0;j<adj[i].size();j++){
-                int index = adj[i][j];
-                int bestPossibleNbr = 0;
-                if(left[index]==-1 and right[index]!=n){
-                    bestPossibleNbr = arr[right[index]];
-                }else if(left[index]!=-1 and right[index]==n){
-                    bestPossibleNbr = arr[left[index]];
-                }else if(left[index]!=-1 and right[index]!=n){
-                    bestPossibleNbr = min(arr[left[index]],arr[right[index]]);
-                }
-                
-                if(left[index]!=-1){
-                    right[left[index]] = right[index];
-                }
-                if(right[index]!=n){
-                    left[right[index]] = left[index];
-                }
-                ans += bestPossibleNbr*i;
+        stack<int> st;
+        int result = 0;
+        for(int x:arr){
+            while(!st.empty() and x>st.top()){
+                int Top = st.top();
+                st.pop();
+                if(st.empty()){
+                    result += Top * x;
+                }else{
+                    result += Top * min(x,st.top());
+                } 
             }
+            st.push(x);
         }
-        return ans;
+        while(st.size()>1){
+            int Top = st.top();
+            st.pop();
+            result += Top*st.top();
+        }
+        return result;
+    
     }
 };
