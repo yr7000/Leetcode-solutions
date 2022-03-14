3eac55222655b0ff0b1df1373b02227dc0c463ab
yr7000 <78471661+yr7000@users.noreply.github.com>
2022-03-14 13:52:48 +0530

    Time: 182 ms (7.34%), Space: 32 MB (5.32%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -1,13 +1,25 @@
 class Solution {
 public:
+    
+    string getHash(string s){
+        string hashValue = "";
+        int cnt[26] = {0};
+        
+        for(int i=0;i<s.size();i++){
+            cnt[s[i]-'a']++;
+        }
+        
+        for(int i=0;i<26;i++){
+            hashValue += to_string(cnt[i]) + (char)(i+'a') + " ";
+        }
+        return hashValue;
+    }
+    
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        map<map<char,int>,vector<string>> mp;
+        map<string,vector<string>> mp;
         for(auto str:strs){
-            map<char,int> mpTemp;
-            for(auto ch:str){
-                mpTemp[ch]++;
-            }
-            mp[mpTemp].push_back(str);
+            string hashValue = getHash(str);
+            mp[hashValue].push_back(str);
         }
         vector<vector<string>> result;
         for(auto ele:mp){
