b2c74402d2e264344b38122476732ea0452583b2
yr7000 <78471661+yr7000@users.noreply.github.com>
2022-03-17 19:45:46 +0530

    Time: 8 ms (91.89%), Space: 21.3 MB (54.40%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.cpp
+++ 968-binary-tree-cameras/968-binary-tree-cameras.cpp
@@ -1,33 +1,30 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     int minCameras = 0;
+    
     string getStateOf(TreeNode* root){
+        // if the root is null, we can treat it as a covered state.
         if(root == NULL) return "covered";
+		
         string leftChildState = getStateOf(root->left);
         string rightChildState = getStateOf(root->right);
+		
+        // if any child is not covered then there must be a camera here to cover them.
         if(leftChildState == "notCovered" or rightChildState == "notCovered"){
             minCameras++;
             return "camera";
         }
+        // if any child has a camera on it then this node is covered.
         if(leftChildState == "camera" or rightChildState == "camera"){
             return "covered";
         }
+        // if the above cases doesnt satisfy, this node's state is not covered.
         return "notCovered";
     }
     
     int minCameraCover(TreeNode* root) {
         string rootState = getStateOf(root);
+        // if the state of root is not covered then we must place a camera on the root node.
         if(rootState == "notCovered"){
             minCameras++;
         }
