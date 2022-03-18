b9cdaa06d812e45eed8c82daddbd3ca9161e6d1b
yr7000 <78471661+yr7000@users.noreply.github.com>
2022-03-18 07:02:09 +0530

    Time: 36 ms (22.99%), Space: 22.3 MB (75.65%) - LeetHub

--- 337-house-robber-iii/337-house-robber-iii.cpp
+++ 337-house-robber-iii/337-house-robber-iii.cpp
@@ -12,21 +12,21 @@
 class Solution {
 public:
     
-    unordered_map<TreeNode*,int> dp;
+    int robHelper(TreeNode* root,int &left_rob,int &right_rob){
+        if(root==NULL) return 0;
+        int left_left_rob = 0, left_right_rob = 0, right_left_rob = 0, right_right_rob = 0;
+        
+        left_rob = robHelper(root->left,left_left_rob,left_right_rob);
+        right_rob = robHelper(root->right,right_left_rob,right_right_rob);
+        
+        int choise1 = root->val + left_left_rob + left_right_rob + right_left_rob + right_right_rob;
+        int choise2 = left_rob + right_rob;
+        
+        return max(choise1,choise2);
+    }
     
     int rob(TreeNode* root) {
-        if(root==NULL) return 0;
-        
-        if(dp.count(root)) return dp[root];
-        
-        int left_left_robbery = root->left ? rob(root->left->left) : 0;
-        int left_right_robbery = root->left ? rob(root->left->right) : 0;
-        int right_left_robbery = root->right ? rob(root->right->left) : 0;
-        int right_right_robbery = root->right ? rob(root->right->right) : 0;
-        
-        int choise1 = root->val + left_left_robbery + left_right_robbery + right_left_robbery + right_right_robbery;
-        int choise2 = rob(root->left) + rob(root->right);
-        
-        return dp[root] = max(choise1,choise2);
+        int left_rob = 0, right_rob = 0;
+        return robHelper(root,left_rob,right_rob);
     }
 };
