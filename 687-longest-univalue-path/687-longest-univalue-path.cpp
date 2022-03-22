2bc94dfb9f6cd7223b7e71553595d606cbbaca50
yr7000 <78471661+yr7000@users.noreply.github.com>
2022-03-22 06:59:06 +0530

    Time: 248 ms (20.05%), Space: 71.8 MB (84.70%) - LeetHub

--- 687-longest-univalue-path/687-longest-univalue-path.cpp
+++ 687-longest-univalue-path/687-longest-univalue-path.cpp
@@ -11,12 +11,23 @@
  */
 class Solution {
 public:
-    
+    // stores the max node count
     int bestPossibleNodeCnt = 1;
+
+    // required path: longest path with same value
+    // No U path: a path where no 2 nodes in the path can have same parent
+    // (No reverse U shape in the path).
+
+    // this function returns: no U required path.
     int longestNoUPath(TreeNode* root){
         if(root==NULL) return 0;
+        // get the no U required path passing through left child
         int l = longestNoUPath(root->left);
+        // get the no U required path passing through right child
         int r = longestNoUPath(root->right);
+        
+        // update the bestPossibleNodeCnt if required
+        // return the longest no u path passing through current tree node.
         if(root->left and root->left->val==root->val and root->right and root->right->val==root->val){
             bestPossibleNodeCnt = max(bestPossibleNodeCnt,1+l+r);
             return 1+max(l,r);
@@ -33,6 +44,7 @@ public:
     
     int longestUnivaluePath(TreeNode* root) {
         longestNoUPath(root);
+        // length = count-1
         int length = bestPossibleNodeCnt-1;
         return length;
     }
