1eb8bdb7bf540184bd9adae99d0b0652864cf004
yr7000 <78471661+yr7000@users.noreply.github.com>
2022-03-31 19:20:24 +0530

    Time: 3 ms (98.52%), Space: 19.9 MB (62.54%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.cpp
+++ 113-path-sum-ii/113-path-sum-ii.cpp
@@ -12,14 +12,20 @@
 class Solution {
 public:
     
-    void getAllPaths(TreeNode* root, int targetSum, vector<int> path, vector<vector<int>> &paths){
+    void getAllPaths(TreeNode* root, int targetSum, vector<int> &path, vector<vector<int>> &paths){
         if(root==NULL) return;
+        // include the current node in the path
         path.push_back(root->val);
+        // if the current node is a leaf node and the path sum is target sum 
+        // add the current path to the result.
         if(root->left==NULL and root->right==NULL and targetSum == root->val){
             paths.push_back(path);
         }
+        // recursively pass the path to the children
         getAllPaths(root->left,targetSum-root->val, path, paths);
         getAllPaths(root->right,targetSum-root->val, path, paths);
+        // as we explored all the paths passing through current node
+        // we can remove this from the path and explore others.
         path.pop_back();
     }
     
