ee7ef88cf0eea2ee2e11a55805a57b5c2cd1c183
yr7000 <78471661+yr7000@users.noreply.github.com>
2022-04-04 19:20:53 +0530

    Time: 1 ms (18.99%), Space: 45.6 MB (44.85%) - LeetHub

--- 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.cpp
+++ 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.cpp
@@ -1,24 +1,27 @@
 /**
  * Definition for a binary tree node.
- * struct TreeNode {
+ * public class TreeNode {
  *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
  */
 class Solution {
-public:
-    TreeNode* trimBST(TreeNode* root, int low, int high) {
-        if(root==NULL) return NULL;
-        root->left = trimBST(root->left,low,high);
-        root->right = trimBST(root->right,low,high);
-        if(root->val>=low and root->val<=high){
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        if(root==null) return null;
+        root.left = trimBST(root.left,low,high);
+        root.right = trimBST(root.right,low,high);
+        if(root.val>=low && root.val<=high){
             return root;
         }
-        if(root->val<low) return root->right;
-        return root->left;
+        if(root.val<low) return root.right;
+        return root.left;        
     }
-};
+}
