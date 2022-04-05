edd352c28cffdd7cb2536e09bb1ca9b1c6cb3f84
yr7000 <78471661+yr7000@users.noreply.github.com>
2022-04-05 19:01:14 +0530

    Time: 1226 ms (5.69%), Space: 7.9 MB (74.83%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     int dx[4][2] = {{0,1},{1,0},{-1,0},{0,-1}};
-    bool dfs(int x,int y,vector<vector<char>>& board,vector<vector<bool>> &visited,string word,int n,int m){
+    bool dfs(int x,int y,vector<vector<char>>& board,string word,int n,int m){
         if(word.size()==0) return true;
-        if(x<0 or y<0 or x>=n or y>=m or visited[x][y] or board[x][y]!=word[0]) return false;
-        visited[x][y] = true;
+        if(x<0 or y<0 or x>=n or y>=m or board[x][y]!=word[0]) return false;
+        char c = board[x][y];
+        board[x][y] = '#';
         for(int i=0;i<4;i++){
             int X = x + dx[i][0];
             int Y = y + dx[i][1];
-            if(dfs(X,Y,board,visited,word.substr(1),n,m)) return true;
+            if(dfs(X,Y,board,word.substr(1),n,m)) return true;
         }
-        visited[x][y] = false;
+        board[x][y] = c;
         return false;
     }
     
@@ -19,9 +20,7 @@ public:
         int m = board[0].size();
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(board[i][j]!=word[0]) continue;
-                vector<vector<bool>> visited(n,vector<bool>(m,false));
-                if(dfs(i,j,board,visited,word,n,m)) return true;
+                if(dfs(i,j,board,word,n,m)) return true;
             }
         }
         return false;
