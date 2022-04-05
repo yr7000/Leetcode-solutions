165b0d4f1cc540e28c9ce3f8b54d8b94acf4b925
yr7000 <78471661+yr7000@users.noreply.github.com>
2022-04-05 18:55:53 +0530

    Time: 1035 ms (11.46%), Space: 8.2 MB (13.72%) - LeetHub

---
+++ 79-word-search/79-word-search.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int dx[4][2] = {{0,1},{1,0},{-1,0},{0,-1}};
+    bool dfs(int x,int y,vector<vector<char>>& board,vector<vector<bool>> &visited,string word,int n,int m){
+        if(word.size()==0) return true;
+        if(x<0 or y<0 or x>=n or y>=m or visited[x][y] or board[x][y]!=word[0]) return false;
+        visited[x][y] = true;
+        for(int i=0;i<4;i++){
+            int X = x + dx[i][0];
+            int Y = y + dx[i][1];
+            if(dfs(X,Y,board,visited,word.substr(1),n,m)) return true;
+        }
+        visited[x][y] = false;
+        return false;
+    }
+    
+    bool exist(vector<vector<char>>& board, string word) {
+        int n = board.size();
+        int m = board[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                vector<vector<bool>> visited(n,vector<bool>(m,false));
+                if(dfs(i,j,board,visited,word,n,m)) return true;
+            }
+        }
+        return false;
+    }
+};
