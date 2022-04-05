d65eb4522d758fddcb3ded76a727cd1f631500a4
yr7000 <78471661+yr7000@users.noreply.github.com>
2022-04-05 18:56:48 +0530

    Time: 1278 ms (5.01%), Space: 8 MB (36.11%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -19,6 +19,7 @@ public:
         int m = board[0].size();
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
+                if(board[i][j]!=word[0]) continue;
                 vector<vector<bool>> visited(n,vector<bool>(m,false));
                 if(dfs(i,j,board,visited,word,n,m)) return true;
             }
