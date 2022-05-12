7c67d6f106ecfc4922da4410f6ccd411942498ad
Yashwanth Raj <78471661+yr7000@users.noreply.github.com>
2022-05-12 19:01:35 +0530

    Time: 99 ms (71.27%), Space: 6 MB (98.44%) - LeetHub

--- 375-guess-number-higher-or-lower-ii/375-guess-number-higher-or-lower-ii.cpp
+++ 375-guess-number-higher-or-lower-ii/375-guess-number-higher-or-lower-ii.cpp
@@ -1,13 +1,22 @@
 class Solution {
 public:
+    
+    // whats your job ?
+    // you have to guess a number
+    // so lets a guess a number between the current range( initally [1,n])
+    // if this my pick then he can say either go to left or go to right 
+    // so we calculate the cost required recursively
+    // max of those 2 values tells the worst cost that may be possible if i start picking the current picked number 
+    // and we choose the min of all the worst cost associated with each pick 
+    
     int memo[201][201];
     int dp(int l, int r){
         if(l>=r) return 0;
         if(memo[l][r]!=-1) return memo[l][r];
         int result = INT_MAX;
-        for(int pick = l; pick <=r; pick++){
-            int leftCost = dp(l,pick-1) + pick;
-            int rightCost = dp(pick+1,r) + pick;
+        for(int guess = l; guess <=r; guess++){
+            int leftCost = dp(l,guess-1) + guess;
+            int rightCost = dp(guess+1,r) + guess;
             int worstPossibleCost = max(leftCost,rightCost);
             result = min(worstPossibleCost,result);
         }
