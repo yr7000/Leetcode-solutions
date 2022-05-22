576f1678cfe510931c96f82e9bad32de779f6b5b
Yashwanth Raj <78471661+yr7000@users.noreply.github.com>
2022-05-22 11:28:52 +0530

    Time: 104 ms (48.07%), Space: 30.2 MB (61.96%) - LeetHub

--- 542-01-matrix/542-01-matrix.cpp
+++ 542-01-matrix/542-01-matrix.cpp
@@ -4,7 +4,6 @@ public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
         queue<pair<int,int>> q;
-        vector<vector<bool>> visited(m,vector<bool>(n));
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==0){
