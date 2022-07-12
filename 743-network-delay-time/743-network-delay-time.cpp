0697d4af2b078a940c57b3d1b21119fb7d08bca4
Yashwanth Raj <78471661+yr7000@users.noreply.github.com>
2022-07-12 18:38:38 +0530

    Time: 227 ms (32.35%), Space: 43.7 MB (24.07%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -21,6 +21,7 @@ public:
             if(visited[currentNode]) continue;
             visited[currentNode] = true;
             for(auto nbr: graph[currentNode]){
+                if(visited[nbr.first]) continue;
                 if(distances[nbr.first] > distanceFromSource + nbr.second){
                     distances[nbr.first] = distanceFromSource + nbr.second;
                     minHeap.push({distances[nbr.first],nbr.first});
